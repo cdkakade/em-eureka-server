79ed4e513725a8e629e95b09333807bf60b35845
cdkakade <cdkakade1@gmail.com>
2024-05-24 14:32:11 +0530

    Initial commit

---
+++ src/test/java/expense/manager/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package expense.manager.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
